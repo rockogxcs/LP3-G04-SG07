f5222e16773fae72881b5932819a0034d7f1c30a
Christian <christian.romero@ucsm.edu.pe>
2023-08-18 20:17:38 -0500

    Se incluyo un import para permitir el uso del Scanner

--- Principal.java
+++ Principal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -7,7 +9,7 @@ public class Principal {
 		Scanner sc = new Scanner(System.in);
 		do {
 			vida ++;
-			System.out.println("Vida:"+ vida + "\t¿Cual es el número secreto? ");
+			System.out.println("Vida:"+ vida + "\t¿Cuál es el número secreto? ");
 			rp = sc.nextInt();
 			if (rp == 1234)
 				fl = true;
