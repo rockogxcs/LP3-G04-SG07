1e03fa8a47d8b5956115dc5402826f603d458688
Christian <christian.romero@ucsm.edu.pe>
2023-08-18 20:24:15 -0500

    Se incluyo un mensaje en el que se muestra las vidas restantes del usuario

--- Principal.java
+++ Principal.java
@@ -7,13 +7,15 @@ public class Principal {
 		boolean fl = false;
 		vida = 0;
 		Scanner sc = new Scanner(System.in);
+		int intentos = 3;
 		do {
 			vida ++;
-			System.out.println("Vida:"+ vida + "\t¿Cuál es el número secreto? ");
+			int vidasrest = intentos - vida + 1;
+			System.out.println("Vida: " + vida + " | Intentos restantes: " + vidasrest + "\t¿Cuál es el número secreto? ");
 			rp = sc.nextInt();
 			if (rp == 1234)
 				fl = true;
-		}while(vida < 3 && !fl );
+		}while(vida < intentos && !fl );
 		if (fl == true)
 			System.out.println("Adivinaste!!!!! ");
 		else
