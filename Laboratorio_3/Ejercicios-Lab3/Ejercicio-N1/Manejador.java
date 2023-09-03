762be467c32cc3d335d87e0d8a34d25bcb4141fc
Christian <christian.romero@ucsm.edu.pe>
2023-09-03 14:29:04 -0500

    Ejercicio 01 - 3 Lab3: Se agregaron metodos a la clase Manejador e instrucciones a la clase Principal.

--- Laboratorio_3/Ejercicios-Lab3/Ejercicio-N1/Manejador.java
+++ Laboratorio_3/Ejercicios-Lab3/Ejercicio-N1/Manejador.java
@@ -1,3 +1,28 @@
 public class Manejador {
 
+    public static double area(Rectangulo r) {
+        int base = r.getEsquina2().getX() - r.getEsquina1().getX();
+        int altura = r.getEsquina2().getY() - r.getEsquina1().getY();
+        return base * altura;
+    }
+
+    public static double perimetro(Rectangulo r) {
+        int base = r.getEsquina2().getX() - r.getEsquina1().getX();
+        int altura = r.getEsquina2().getY() - r.getEsquina1().getY();
+        return 2 * (base + altura);
+    }
+
+    public static void moverX(Rectangulo r, int puntos) {
+        Coordenada nuevaEsquina1 = new Coordenada(r.getEsquina1().getX() + puntos, r.getEsquina1().getY());
+        Coordenada nuevaEsquina2 = new Coordenada(r.getEsquina2().getX() + puntos, r.getEsquina2().getY());
+        r.setEsquina1(nuevaEsquina1);
+        r.setEsquina2(nuevaEsquina2);
+    }
+
+    public static void moverY(Rectangulo r, int puntos) {
+        Coordenada nuevaEsquina1 = new Coordenada(r.getEsquina1().getX(), r.getEsquina1().getY() + puntos);
+        Coordenada nuevaEsquina2 = new Coordenada(r.getEsquina2().getX(), r.getEsquina2().getY() + puntos);
+        r.setEsquina1(nuevaEsquina1);
+        r.setEsquina2(nuevaEsquina2);
+    }
 }
--- Laboratorio_3/Ejercicios-Lab3/Ejercicio-N1/Principal.java
+++ Laboratorio_3/Ejercicios-Lab3/Ejercicio-N1/Principal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la coordenada X del rectángulo: ");
+        int x = sc.nextInt();
+        
+        System.out.print("Ingrese la coordenada Y del rectángulo: ");
+        int y = sc.nextInt();
+        
+        Rectangulo r1 = new Rectangulo(new Coordenada(), new Coordenada(x, y));
+        System.out.println("\n" + r1);
+        System.out.println("Area: " + Manejador.area(r1));
+        System.out.println("Perimetro: " + Manejador.perimetro(r1));
+        
+        System.out.print("¿Cuantas puntos desea mover el rectángulo en el eje X?: ");
+        int puntosX = sc.nextInt();
 
+        Manejador.moverX(r1, puntosX);
+        
+        System.out.print("¿Cuantas puntos desea mover el rectángulo en el eje Y?: ");
+        int puntosY = sc.nextInt();
+        
+        Manejador.moverY(r1, puntosY);
+        System.out.println("\nSe realizaron los cambios: ");
+        System.out.println(r1);
     }
 }
