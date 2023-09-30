0ca9fbdd15993c1cb64c508e08fef15e5b253677
Christian <christian.romero@ucsm.edu.pe>
2023-09-30 11:53:45 -0500

    Actividad 05: Se agregaron las clases abstractas FiguraGeometrica, Rectangulo y TestAbstracto

---
+++ Laboratorio_5/Actividades-Lab5/Actividad-05-Abstractas/FiguraGeometrica.java
@@ -0,0 +1,16 @@
+public abstract class FiguraGeometrica {
+    private String nombre;
+    
+    abstract public double area();
+    public FiguraGeometrica(String nombreFigura ) {
+        this.nombre = nombreFigura;
+    }
+    
+    final public boolean mayorQue (FiguraGeometrica otra) {
+        return this.area()>otra.area();
+    }
+    
+    final public String toString(){
+        return this.nombre + " con area " + this.area();
+    }
+}
---
+++ Laboratorio_5/Actividades-Lab5/Actividad-05-Abstractas/Rectangulo.java
@@ -0,0 +1,14 @@
+public class Rectangulo extends FiguraGeometrica {
+    private double base;
+    private double altura;
+    
+    public Rectangulo (double largo, double ancho) {
+        super("Rectangulo");
+        this.base = largo;
+        this.altura = ancho;
+    }
+    
+    public double area () {
+        return this.base * this.altura;
+    }
+}
