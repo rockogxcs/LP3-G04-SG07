7abc165ffe1200d739de4bbda0eb3c75ea632585
Christian <christian.romero@ucsm.edu.pe>
2023-10-02 23:08:20 -0500

    Ejercicio 01: Se modifico la clase TestComposicion para mostrar los resultados en un menu de opciones

--- Laboratorio_5/Ejercicios-Lab5/Ejercicio-01/TestComposicion.java
+++ Laboratorio_5/Ejercicios-Lab5/Ejercicio-01/TestComposicion.java
@@ -1,40 +1,135 @@
-public class TestComposicion{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestComposicion {
     public static void main(String[] args) {
-        Persona p1 = new Persona(2734, "Oscar", "Gutierrez", 'C');
-        System.out.println(p1.toString());
-        System.out.println("\n");
+        Scanner sc = new Scanner(System.in);
+        List<Persona> clientes = new ArrayList<>();
 
-        p1.getCuenta().depositar(2500);   // Deposita 2500 soles a Oscar (p1)
-        System.out.println("\tSe depositaron S/.2500 a Oscar");
-        System.out.println(p1.toString());
-        System.out.println("\n");
+        while (true) {
+            System.out.println("1. Añadir cliente");
+            System.out.println("2. Ver información de la cuenta");
+            System.out.println("3. Depositar");
+            System.out.println("4. Retirar");
+            System.out.println("5. Salir");
+            System.out.print("Ingrese una opción: ");
+            int opcion = sc.nextInt();
 
-        Persona p2 = new Persona(2735, "Alfonso", "Mateluna", 'B');
-        System.out.println(p2.toString());
-        System.out.println("\n");
+            switch (opcion) {
+                case 1:
+                    System.out.print("Ingrese el ID del cliente: ");
+                    int id = sc.nextInt();
+                    System.out.print("Ingrese el nombre del cliente: ");
+                    String nombre = sc.next();
+                    System.out.print("Ingrese el apellido del cliente: ");
+                    String apellido = sc.next();
+                    System.out.print("¿Desea ingresar el tipo de cliente? (S/N): ");
+                    char rpta = sc.next().toUpperCase().charAt(0);
+                    char tipo = 'C';
 
-        p2.getCuenta().depositar(5000);   // Deposita 5000 soles a Alfonso (p2)
-        System.out.println("\tSe depositaron S/.5000 a Alfonso");
-        System.out.println(p2.toString());
-        System.out.println("\n");
+                    if (rpta == 'S') {
+                        System.out.print("Ingrese el tipo de cliente (C, B, E): ");
+                        tipo = sc.next().toUpperCase().charAt(0);
+                    }
+                    Persona persona = new Persona(id, nombre, apellido, tipo);
+                    clientes.add(persona);
+                    break;
+                case 2:
+                    if (clientes.isEmpty()) {
+                        System.out.println("No hay clientes registrados.");
+                        break;
+                    }
+                    System.out.println("\n\tLista de clientes: ");
+                    for (int i = 0; i < clientes.size(); i++) {
+                        System.out.println(clientes.get(i).getId() + ". \t" + clientes.get(i).getNombre() + " " + clientes.get(i).getApellido());
+                    }
+                    System.out.print("\nIngrese el ID del cliente para ver información de la cuenta: ");
+                    int clienteId = sc.nextInt();
+                    Persona clienteSeleccionado = null;
 
-        Persona p3 = new Persona(2736, "Angela", "Gomez", 'E');
-        System.out.println(p3.toString());
-        System.out.println("\n");
+                    for (Persona cliente : clientes) {
+                        if (cliente.getId() == clienteId) {
+                            clienteSeleccionado = cliente;
+                            break;
+                        }
+                    }
 
-        p3.getCuenta().depositar(8000);   // Deposita 8000 soles a Angela (p3)
-        System.out.println("\tSe depositaron S/.8000 a Angela");
-        System.out.println(p3.toString());
-        System.out.println("\n");
-        
-        p2.getCuenta().retirar(300);   // Se retiran 300 soles a Alfonso (p2)
-        System.out.println("\tSe retiraron S/.300 a Alfonso");
-        System.out.println(p2.toString());
-        System.out.println("\n");
-        
-        p1.getCuenta().retirar(-20);   // Se intenta retiran -20 soles a Oscar (p1)
-        System.out.println("\tNo se pudo retirar el dinero a Oscar");
-        System.out.println(p1.toString());
-        System.out.println("\n");
+                    if (clienteSeleccionado == null) {
+                        System.out.println("Cliente no encontrado.");
+                    } else {
+                        System.out.print("\n");
+                        System.out.println(clienteSeleccionado.toString());
+                        System.out.print("\n");
+                    }
+                    break;
+
+                case 3:
+                    if (clientes.isEmpty()) {
+                        System.out.println("No hay clientes registrados.");
+                        break;
+                    }
+                    System.out.println("\n\tLista de clientes: ");
+                    for (int i = 0; i < clientes.size(); i++) {
+                        System.out.println(clientes.get(i).getId() + ". \t" + clientes.get(i).getNombre() + " " + clientes.get(i).getApellido());
+                    }
+                    System.out.print("\nIngrese el ID del cliente para depositar: ");
+                    clienteId = sc.nextInt();
+                    clienteSeleccionado = null;
+
+                    for (Persona cliente : clientes) {
+                        if (cliente.getId() == clienteId) {
+                            clienteSeleccionado = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteSeleccionado == null) {
+                        System.out.println("Cliente no encontrado.");
+                    } else {
+                        System.out.print("Ingrese la cantidad a depositar: ");
+                        double deposito = sc.nextDouble();
+                        clienteSeleccionado.getCuenta().depositar(deposito);
+                        System.out.println("Depósito realizado con éxito.");
+                    }
+                    break;
+
+                case 4:
+                    if (clientes.isEmpty()) {
+                        System.out.println("No hay clientes registrados.");
+                        break;
+                    }
+                    System.out.println("\n\tLista de clientes: ");
+                    for (int i = 0; i < clientes.size(); i++) {
+                        System.out.println(clientes.get(i).getId() + ". " + clientes.get(i).getNombre() + " " + clientes.get(i).getApellido());
+                    }
+                    System.out.print("\nIngrese el ID del cliente para retirar: ");
+                    clienteId = sc.nextInt();
+                    clienteSeleccionado = null;
+
+                    for (Persona cliente : clientes) {
+                        if (cliente.getId() == clienteId) {
+                            clienteSeleccionado = cliente;
+                            break;
+                        }
+                    }
+
+                    if (clienteSeleccionado == null) {
+                        System.out.println("Cliente no encontrado.");
+                    } else {
+                        System.out.print("Ingrese la cantidad a retirar: ");
+                        double retiro = sc.nextDouble();
+                        clienteSeleccionado.getCuenta().retirar(retiro);
+                        System.out.println("Retiro realizado con éxito.");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("Saliendo...");
+                    return;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        }
     }
 }
