5bd55e7900ec7e62f011b28bb42b6b41de68a075
Christian <christian.romero@ucsm.edu.pe>
2023-10-22 15:31:36 -0500

    Ejercicio 1: Se agrego el programa MainEjercicio1 para probar la primera invocacion

---
+++ Laboratorio_7/Ejercicios-Lab7/Ejercicio-N1/MainEjercicio1.java
@@ -0,0 +1,18 @@
+public class MainEjercicio1{
+	public static void main(String[] args) {
+		double [] v = new double[15];
+		acceso(v, 16);
+	}
+	
+	public static double acceso(double[] v, int j) throws Exception {
+	    try{
+	        if(j >= 0 && j <= v.length)
+	            return v[j];
+	        else
+	            throw new Exception("El indice " + j + " no existe en el vector");
+	    }
+	    catch(RuntimeException exc){
+	        throw exc;
+	    }
+	}
+}
