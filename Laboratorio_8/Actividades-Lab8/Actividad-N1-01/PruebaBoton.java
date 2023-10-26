27c9a71262dad578ae5d54c4aa5d30cbe639c442
Christian <christian.romero@ucsm.edu.pe>
2023-10-26 18:59:59 -0500

    Actividad-N1-01: Se agregaron las clases MarcoBoton y PruebaBoton para probar los botones de comando y eventos de accion

---
+++ Laboratorio_8/Actividades-Lab8/Actividad-N1-01/PruebaBoton.java
@@ -0,0 +1,14 @@
+// Fig. 12.16: PruebaBoton.java
+// Prueba de MarcoBoton.
+import javax.swing.JFrame;
+
+public class PruebaBoton
+{
+    public static void main(String[] args)
+    {
+        MarcoBoton marcoBoton = new MarcoBoton();
+        marcoBoton.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoBoton.setSize(275, 110);
+        marcoBoton.setVisible(true);
+    }
+} // fin de la clase PruebaBoton
