fdf30963fbf46ed2acb7d15bd7b4663d77c12efd
Christian <christian.romero@ucsm.edu.pe>
2023-10-27 13:36:18 -0500

    Actividad-N2-03: Se agregaron las clases MarcoGridLayout y DemoGrindLayout que contene seis botones para probar la redistribucion de un contenedor

---
+++ Laboratorio_8/Actividades-Lab8/Actividad-N2-03/DemoGridLayout.java
@@ -0,0 +1,14 @@
+// Fig. 12.44: DemoGridLayout.java
+// Prueba de MarcoGridLayout.
+import javax.swing.JFrame;
+
+public class DemoGridLayout
+{
+    public static void main(String[] args)
+    {
+        MarcoGridLayout marcoGridLayout = new MarcoGridLayout();
+        marcoGridLayout.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoGridLayout.setSize(300, 200);
+        marcoGridLayout.setVisible(true);
+    }
+} // fin de la clase DemoGridLayout
