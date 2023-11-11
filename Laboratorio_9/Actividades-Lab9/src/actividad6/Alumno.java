bb30cd1edbb641d26b9b0181a982e21012604231
Christian <christian.romero@ucsm.edu.pe>
2023-11-11 16:55:57 -0500

    Actividad 6: Se agrego las clases para el programa que escribe en un archivo tres objetos de tipo Alumno

---
+++ Laboratorio_9/Actividades-Lab9/src/actividad6/Alumno.java
@@ -0,0 +1,24 @@
+package actividad6;
+
+public class Alumno extends Persona {
+    private Fecha fechaMatricula;
+
+    public Alumno(String nif, String nombre, int edad, Fecha fechaMatricula) {
+        super(nif, nombre, edad);
+        this.fechaMatricula = new Fecha();
+        setFechaMatricula(fechaMatricula);
+    }
+
+    public Alumno() {
+    }
+
+    public Fecha getFechaMatricula() {
+        return fechaMatricula;
+    }
+
+    public void setFechaMatricula(Fecha fechaMatricula) {
+        this.fechaMatricula.setDia(fechaMatricula.getDia());
+        this.fechaMatricula.setMes(fechaMatricula.getMes());
+        this.fechaMatricula.setAño(fechaMatricula.getAño());
+    }
+}
